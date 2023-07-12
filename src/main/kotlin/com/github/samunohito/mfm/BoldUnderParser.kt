b8bec63c37c7ec2045013af2a2e08264af2ecf0d
osamu <46447427+sam-osamu@users.noreply.github.com>
2023-07-12 20:57:16 +0900

    support bold under

---
+++ src/main/kotlin/com/github/samunohito/mfm/BoldUnderParser.kt
@@ -0,0 +1,33 @@
+@file:Suppress("DuplicatedCode")
+
+package com.github.samunohito.mfm
+
+import com.github.samunohito.mfm.internal.core.AlternateScanFinder
+import com.github.samunohito.mfm.internal.core.RegexFinder
+import com.github.samunohito.mfm.internal.core.SequentialFinder
+import com.github.samunohito.mfm.internal.core.StringFinder
+import com.github.samunohito.mfm.internal.core.singleton.SpaceFinder
+import com.github.samunohito.mfm.node.MfmBold
+import com.github.samunohito.mfm.node.MfmText
+
+class BoldUnderParser : IParser<MfmBold> {
+  companion object {
+    private val markFinder = StringFinder("__")
+    private val italicUnderFinder = SequentialFinder(
+      markFinder,
+      AlternateScanFinder.ofWhile(RegexFinder(Regex("[a-z0-9]", RegexOption.IGNORE_CASE)), SpaceFinder),
+      markFinder
+    )
+  }
+
+  override fun parse(input: String, startAt: Int): ParserResult<MfmBold> {
+    val result = italicUnderFinder.find(input, startAt)
+    if (!result.success) {
+      return ParserResult.ofFailure()
+    }
+
+    val contents = input.substring(result.subResults[1].range)
+    val textNode = MfmText(contents)
+    return ParserResult.ofSuccess(MfmBold(listOf(textNode)), input, result.range, result.next)
+  }
+}
