e51e33c384bff126f3151cdcec74ba0038cb47e8
osamu <46447427+sam-osamu@users.noreply.github.com>
2023-07-12 21:07:13 +0900

    support math block

---
+++ src/main/kotlin/com/github/samunohito/mfm/MathBlockParser.kt
@@ -0,0 +1,37 @@
+package com.github.samunohito.mfm
+
+import com.github.samunohito.mfm.internal.core.SequentialFinder
+import com.github.samunohito.mfm.internal.core.SequentialScanFinder
+import com.github.samunohito.mfm.internal.core.StringFinder
+import com.github.samunohito.mfm.internal.core.singleton.LineBeginFinder
+import com.github.samunohito.mfm.internal.core.singleton.LineEndFinder
+import com.github.samunohito.mfm.internal.core.singleton.NewLineFinder
+import com.github.samunohito.mfm.node.MfmMathBlock
+
+class MathBlockParser : IParser<MfmMathBlock> {
+  companion object {
+    private val open = StringFinder("\\[")
+    private val close = StringFinder("\\]")
+    private val mathBlockFinder = SequentialFinder(
+      NewLineFinder.optional(),
+      LineBeginFinder,
+      open,
+      NewLineFinder.optional(),
+      SequentialScanFinder.ofUntil(NewLineFinder.optional(), close),
+      NewLineFinder.optional(),
+      close,
+      LineEndFinder,
+      NewLineFinder.optional()
+    )
+  }
+
+  override fun parse(input: String, startAt: Int): ParserResult<MfmMathBlock> {
+    val result = mathBlockFinder.find(input, startAt)
+    if (!result.success) {
+      return ParserResult.ofFailure()
+    }
+
+    val formula = input.substring(result.subResults[4].range)
+    return ParserResult.ofSuccess(MfmMathBlock(formula), input, result.range, result.next)
+  }
+}
--- src/main/kotlin/com/github/samunohito/mfm/node/MfmMathBlock.kt
+++ src/main/kotlin/com/github/samunohito/mfm/node/MfmMathBlock.kt
@@ -4,5 +4,7 @@ data class MfmMathBlock(override val props: Props) :
   IMfmBlock<MfmMathBlock.Props> {
   override val type = MfmNodeType.MathBlock
 
+  constructor(formula: String) : this(Props(formula))
+
   data class Props(val formula: String) : IMfmProps
 }
