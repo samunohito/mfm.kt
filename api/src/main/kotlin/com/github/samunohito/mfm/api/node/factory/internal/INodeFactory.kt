7b9b4642def3d42f45102f7a2c45af3f1258498b
osamu <46447427+sam-osamu@users.noreply.github.com>
2023-08-02 09:25:00 +0900

    add comment

--- api/src/main/kotlin/com/github/samunohito/mfm/api/node/factory/internal/INodeFactory.kt
+++ api/src/main/kotlin/com/github/samunohito/mfm/api/node/factory/internal/INodeFactory.kt
@@ -1,8 +1,19 @@
 package com.github.samunohito.mfm.api.node.factory.internal
 
 import com.github.samunohito.mfm.api.node.IMfmNode
+import com.github.samunohito.mfm.api.parser.IMfmParser
 import com.github.samunohito.mfm.api.parser.SubstringFoundInfo
 
+/**
+ * Create an object of [IMfmNode] from the parsing result of [IMfmParser].
+ */
 interface INodeFactory<T : IMfmNode> {
+  /**
+   * Create an object of [IMfmNode] from the parsing result of [IMfmParser].
+   *
+   * @param input The input string.
+   * @param foundInfo The parsing result of [IMfmParser].
+   * @param context The context of the factory.
+   */
   fun create(input: String, foundInfo: SubstringFoundInfo, context: INodeFactoryContext): IFactoryResult<T>
 }
--- api/src/main/kotlin/com/github/samunohito/mfm/api/node/factory/internal/INodeFactoryContext.kt
+++ api/src/main/kotlin/com/github/samunohito/mfm/api/node/factory/internal/INodeFactoryContext.kt
@@ -1,11 +1,24 @@
 package com.github.samunohito.mfm.api.node.factory.internal
 
+import com.github.samunohito.mfm.api.node.MfmText
+
+/**
+ * The context of the factory.
+ */
 interface INodeFactoryContext {
+  /**
+   * The maximum nest level of the factory.
+   */
   val maximumNestLevel: Int
+
+  /**
+   * The nest level of the factory.
+   * If this number exceeds [maximumNestLevel], all parsing results below that level will be treated as [MfmText] nodes.
+   */
   var nestLevel: Int
 
   data class Impl(
     override val maximumNestLevel: Int = 20,
-    override var nestLevel: Int = 0
+    override var nestLevel: Int = 0,
   ) : INodeFactoryContext
 }
